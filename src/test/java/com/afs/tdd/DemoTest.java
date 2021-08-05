a5195e80bf6321c41a159080fd10d501008b750a
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-05 21:27:25 +0800

    [MarsRover][ZULUEJA] test: added test case 6 for mars rover

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -72,4 +72,16 @@ class DemoTest {
         assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
         assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
     }
+
+    @Test
+    void should_return_locationX_0_locationY_0_direction_W_when_execute_command_given_locationX_0_locationY_0_direction_S_and_R() {
+        Application marsRover = new Application(new RoverStatus(0,0,"S"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,0,"W");
+
+        marsRover.executeCommand("R");
+
+        assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
 }
